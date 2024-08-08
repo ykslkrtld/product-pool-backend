60f2849acc12269bf754e24a68006e2b0bc22085
yksl <yukselkurtuldu9@gmail.com>
2024-08-08 20:44:20 +0300

    first commit

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,10 @@
+"use strict"
+/* -------------------------------------------------------
+    | FULLSTACK TEAM | NODEJS / EXPRESS |
+------------------------------------------------------- */
+// app.use(authentication):
+
+module.exports = async (req, res, next) => {
+
+    next()
+}
