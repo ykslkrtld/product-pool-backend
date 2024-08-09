955e6db99fa6a27078431800a9d59da362822d32
yksl <yukselkurtuldu9@gmail.com>
2024-08-09 12:12:58 +0300

    purchase

--- src/controllers/purchase.js
+++ src/controllers/purchase.js
@@ -2,3 +2,51 @@
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const Purchase = require('../models/purchase')
+
+module.exports = {
+
+    list: async (req, res) => {
+        const data = await res.getModelList(Purchase)
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Purchase),
+            data,
+          });
+    },
+    create: async (req, res) => {
+        const data = await Purchase.create(req.body)
+
+        res.status(201).send({
+            error: false,
+            data,
+          });
+    },
+    read: async (req, res) => {
+        const data = await Purchase.findOne({ _id: req.params.id });
+
+        res.status(200).send({
+            error: false,
+            data,
+          });
+    },
+    update: async (req, res) => {
+        const data = await Purchase.updateOne({ _id: req.params.id }, req.body, {runValidators: true})
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Purchase.findOne({ _id: req.params.id }),
+          });
+    },
+    delete: async (req, res) => {
+        const data = await Purchase.deleteOne({ _id: req.params.id });
+
+        res.status(data.deletedCount ? 204 : 404).send({
+          error: !data.deletedCount,
+          data,
+    });
+    }
+}
--- src/models/purchase.js
+++ src/models/purchase.js
@@ -3,4 +3,46 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+
 /* ------------------------------------------------------- */
+
+const PurchaseSchema = new mongoose.Schema(
+    {
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    firmId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Firm',
+        required: true
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    brandId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },  
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    priceTotal: {
+        type: Number,
+        // required: true
+    }
+    },
+    { collection: "purchases", timestamps: true },
+);
+
+
+module.exports = mongoose.model("Purchase", PurchaseSchema)
