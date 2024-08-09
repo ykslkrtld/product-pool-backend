e8fa4584363ff397989a0d3818f09c5e8017be1e
yksl <yukselkurtuldu9@gmail.com>
2024-08-09 11:59:56 +0300

    product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -2,3 +2,51 @@
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+const Product = require('../models/product')
+
+module.exports = {
+
+    list: async (req, res) => {
+        const data = await res.getModelList(Product)
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Product),
+            data,
+          });
+    },
+    create: async (req, res) => {
+        const data = await Product.create(req.body)
+
+        res.status(201).send({
+            error: false,
+            data,
+          });
+    },
+    read: async (req, res) => {
+        const data = await Product.findOne({ _id: req.params.id });
+
+        res.status(200).send({
+            error: false,
+            data,
+          });
+    },
+    update: async (req, res) => {
+        const data = await Product.updateOne({ _id: req.params.id }, req.body, {runValidators: true})
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Product.findOne({ _id: req.params.id }),
+          });
+    },
+    delete: async (req, res) => {
+        const data = await Product.deleteOne({ _id: req.params.id });
+
+        res.status(data.deletedCount ? 204 : 404).send({
+          error: !data.deletedCount,
+          data,
+    });
+    }
+}
--- src/models/product.js
+++ src/models/product.js
@@ -3,4 +3,34 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+
 /* ------------------------------------------------------- */
+
+const ProductSchema = new mongoose.Schema(
+    {
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    brandId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },  
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    }
+    },
+    { collection: "firms", timestamps: true },
+);
+
+
+module.exports = mongoose.model("Product", ProductSchema)
