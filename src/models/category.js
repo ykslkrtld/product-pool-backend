84895f96ef7acb92444c92c5e82a2e63b17b6a1c
yksl <yukselkurtuldu9@gmail.com>
2024-08-09 11:24:38 +0300

    category

--- src/models/category.js
+++ src/models/category.js
@@ -3,4 +3,20 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+
 /* ------------------------------------------------------- */
+
+const CategorySchema = new mongoose.Schema(
+    {
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    }
+    },
+    { collection: "brands", timestamps: true },
+);
+
+
+module.exports = mongoose.model("Category", CategorySchema)
