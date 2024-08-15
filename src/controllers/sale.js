c2ac141f6e4521d15bad7e0d4580e15eceab99d0
yksl <yukselkurtuldu9@gmail.com>
2024-08-15 19:49:27 +0300

    sale

--- src/controllers/sale.js
+++ src/controllers/sale.js
@@ -31,46 +31,40 @@ module.exports = {
     },
 
     create: async (req, res) => {
-        /*
-            #swagger.tags = ["Sales"]
-            #swagger.summary = "Create Sale"
-            #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                    "userId": "65343223gg4e9681f937f107",
-                    "brandId": "65343222b67e9681f937f107",
-                    "productId": "65343222b67e9681f937f422",
-                    "quantity": 1,
-                    "price": 9.99
-                },
-            }
-        */
-       req.body.amount = req.body.price * req.body.quantity
-
-       // Set userId from logined user:
-        req.body.userId = req.user._id
-
-        // Güncel stok bilgisini al:
-        const currentProduct = await Product.findOne({ _id: req.body.productId })
-
+        req.body.amount = req.body.price * req.body.quantity;
+    
+        // Set userId from logged-in user:
+        req.body.userId = req.user._id;
+    
+        // Mevcut ürünün bilgilerini al:
+        const currentProduct = await Product.findOne({ _id: req.body.productId });
+    
+        if (!currentProduct) {
+            res.errorStatusCode = 404;
+            throw new Error('Product not found.');
+        }
+    
+        // BrandId kontrolü:
+        if (currentProduct.brandId.toString() !== req.body.brandId) {
+            res.errorStatusCode = 422;
+            throw new Error('The product with the specified brandId does not exist.');
+        }
+    
+        // Stok kontrolü:
         if (currentProduct.stock >= req.body.quantity) {
-
-            // Create:
-            const data = await Sale.create(req.body)
-
+            // Create sale:
+            const data = await Sale.create(req.body);
+    
             // Satıştan sonra product adetten eksilt:
-            const updateProduct = await Product.updateOne({ _id: data.productId }, { $inc: { stock: -data.quantity } })
-
+            await Product.updateOne({ _id: currentProduct._id }, { $inc: { stock: -req.body.quantity } });
+    
             res.status(201).send({
                 error: false,
                 data
-            })
-
+            });
         } else {
-
-            res.errorStatusCode = 422
-            throw new Error('There is not enough product-quantity for this sale.', { cause: { currentProduct } })
+            res.errorStatusCode = 422;
+            throw new Error('There is not enough product-quantity for this sale.');
         }
     },
 
@@ -88,9 +82,9 @@ module.exports = {
     },
 
     update: async (req, res) => {
-         /*
+        /*
             #swagger.tags = ["Sales"]
-            #swagger.summary = "Create Sale"
+            #swagger.summary = "Update Sale"
             #swagger.parameters['body'] = {
                 in: 'body',
                 required: true,
@@ -103,34 +97,52 @@ module.exports = {
                 }
             }
         */
+        
+        const currentSale = await Sale.findOne({ _id: req.params.id });
+        
+        if (!currentSale) {
+            return res.status(404).send({
+                error: true,
+                message: "Sale not found"
+            });
+        }
+    
+        // `productId` veya `brandId`'de değişiklik yapılmaya çalışılıyorsa hata ver:
+        if ((req.body.productId && req.body.productId !== currentSale.productId.toString()) || (req.body.brandId && req.body.brandId !== currentSale.brandId.toString())) {
+            return res.status(400).send({
+                error: true,
+                message: "Changing the productId/brandId is not allowed."
+            });
+        }
+
+        // Eğer `price` veya `quantity` değişiyorsa `amount`'u tekrar hesapla
         if (req.body.price && req.body.quantity) {
-            req.body.amount = req.body.price * req.body.quantity
+            req.body.amount = req.body.price * req.body.quantity;
         }
-
+    
+        // Quantity farkı hesaplama ve stok güncellemesi
         if (req.body?.quantity) {
-            // Mevcut işlemdeki adet bilgisi al:
-            const currentSale = await Sale.findOne({ _id: req.params.id })
-            // Farkı hesapla:
-            const difference = req.body.quantity - currentSale.quantity
-            // Farkı Producta yansıt:
-            const updateProduct = await Product.updateOne({ _id: currentSale.productId, stock: { $gte: difference } }, { $inc: { stock: -difference } })
-            // Miktar yeterli değilse hataya yönlendir:
-            if (updateProduct.modifiedCount == 0) {
-                res.errorStatusCode = 422
-                throw new Error('There is not enough product-quantity for this sale.')
+            const difference = req.body.quantity - currentSale.quantity;
+            const updateProduct = await Product.updateOne(
+                { _id: currentSale.productId, stock: { $gte: difference } },
+                { $inc: { stock: -difference } }
+            );
+            if (updateProduct.modifiedCount === 0) {
+                res.errorStatusCode = 422;
+                throw new Error("There is not enough product-quantity for this sale.");
             }
-            // productId değişmemeli:
-            req.body.productId = currentSale.productId
         }
-        // Update:
-        const data = await Sale.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
-
+    
+        // Güncelleme işlemi
+        const data = await Sale.updateOne({ _id: req.params.id }, req.body, { runValidators: true });
+    
         res.status(202).send({
             error: false,
             data,
             new: await Sale.findOne({ _id: req.params.id }).populate(['userId', 'brandId', 'productId'])
-        })
+        });
     },
+    
 
     delete: async (req, res) => {
            /*
